279ea3657b4590c8df3fb27a905a35c6192ab2a6
Adam J. Weigold <adam@adamweigold.com>
2016-02-07 09:37:53 -0600

    Fixing signing env var

--- .travis/upload_nexus.sh
+++ .travis/upload_nexus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PUBLISH=true
-./gradlew uploadArchives -Pversion=1.0.$TRAVIS_BUILD_NUMBER -Psigning.password=$signing.password -PsonatypeUsername=$sonatypeUsername -PsonatypePassword=$sonatypePassword
+./gradlew uploadArchives -Pversion=1.0.$TRAVIS_BUILD_NUMBER -Psigning.password=$signingPassword -PsonatypeUsername=$sonatypeUsername -PsonatypePassword=$sonatypePassword
