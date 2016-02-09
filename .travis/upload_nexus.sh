c4ece53c7c8ee7e640077f540d98384ceb28b4e9
Adam J. Weigold <adam@adamweigold.com>
2016-02-09 00:57:33 -0600

    Adding automatic close and release in central

--- .travis/upload_nexus.sh
+++ .travis/upload_nexus.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export PUBLISH=true
 ./gradlew uploadArchives -Psigning.password=$signingPassword -PsonatypeUsername=$sonatypeUsername -PsonatypePassword=$sonatypePassword
+./gradlew closeAndPromoteRepository -PnexusUsername=$sonatypeUsername -PnexusPassword=$sonatypePassword
